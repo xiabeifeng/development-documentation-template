b84956133086532f65b2e2854303d12d51a07c6a
= <840679824@qq.com>
2016-04-15 11:38:08 +0800

    js代码结构调整

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 $(function(){
-	$('.portal').click(function(){
-		$(this).siblings('.portal').find('.body').css('display', 'none');
-		$(this).find('.body').toggle(500);
-	});
+    // 页面初始化
+    $.getJSON("./config/config.json", function(data){
+        $('title').html(data.title);
+        $('#favicon').attr('href', './image/'+data.favicon);
+        $('#headerTop').css('border-top', data.border_top_style);
+        $('#logoBox').find('img').attr('src', './image/'+data.logo);
+        $('#logoBox').find('img').attr('alt', data.title);
+        $('#footer').find('p').html(data.copyright);
+    });
+    
+    $.get("./html/update.html", function(result){
+        // $("#content").html(result);
+    });
+	
+    $('.portal').click(function(){
+        $(this).siblings('.portal').find('.body').css('display', 'none');
+        $(this).find('.body').toggle(500);
+    });
 });
